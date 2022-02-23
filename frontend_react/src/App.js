29fee3049fd0027219b6501c909e4c20cf913e91
Nez <checkman3335@gmail.com>
2022-02-22 18:58:31 -0800

    import components, containers, and scss

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+//import everything instead one each with 
+//import About from './container/About'
+
+import { About, Footer, Header, Skills, Testimonials, Work} from './container/index.js';
+import { Navbar } from './components/index.js';
+
+import './App.scss'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className ="app">
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonials />
+        <Footer />
+    </div>
   )
 }
 
